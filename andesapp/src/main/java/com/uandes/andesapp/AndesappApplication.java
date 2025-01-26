638660ef15ca6576a5e56b66f47b5e935b0b9853
franarayah <franarayah@gmail.com>
2025-01-26 14:22:02 -0300

    Se establece la configuracion inicial del proyecto

---
+++ andesapp/src/main/java/com/uandes/andesapp/AndesappApplication.java
@@ -0,0 +1,13 @@
+package com.uandes.andesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndesappApplication.class, args);
+	}
+
+}
---
+++ andesapp/src/test/java/com/uandes/andesapp/AndesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uandes.andesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
